0aae5ce41240de2e6e2438f63c2711c6b7d35184
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2024-04-04 18:16:38 +0200

    check number of hotelNames

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class HotelSearch {
 
@@ -20,11 +22,27 @@ public class HotelSearch {
         driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
         driver.findElement(By.xpath("//*[@id=\"select2-drop\"]/div/input")).sendKeys("Dubai");
         driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
-        // set data
+        // set checkin
         driver.findElement(By.name("checkin")).sendKeys("14/09/2024");
 //        driver.findElement(By.xpath("//*[@id=\"dpd1\"]/div/input")).sendKeys("13/09/2024");
+        // set checkout
         driver.findElement(By.name("checkout")).sendKeys("21/09/2024");
 //        driver.findElement(By.xpath("//*[@id=\"dpd2\"]/div/input")).sendKeys("20/09/2024");
+        driver.findElement(By.id("travellersInput")).click();
+        // add adult
+        driver.findElement(By.id("adultPlusBtn")).click();
+        // add child
+        driver.findElement(By.id("childPlusBtn")).click();
+        // click search
+//        driver.findElement(By.xpath("//*[@id=\"hotels\"]/form/div[5]/button")).click();
+        driver.findElement(By.xpath("//button[text()=' Search']")).click();
+
+        List<String> hotelNames = driver.findElements(By.xpath(
+                "//h4[contains(@class,'list_title')]//b")).stream()
+                                    .map(el -> el.getText()).collect(Collectors.toList());
+        System.out.println(hotelNames.size());
+
+
 
 
 
