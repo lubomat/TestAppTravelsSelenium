a1ad3ce41e60b0bb9211431f18e77f6979eba562
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2024-04-05 13:55:23 +0200

    moving methods to SignUpTest and HotelSearch and removing classes

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -60,12 +60,43 @@ public class HotelSearch {
 
         driver.close();
 
+    }
 
+    @Test
+    public void searchHotelWithoutCity() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(2L, TimeUnit.SECONDS);
+        driver.get("http://kurs-selenium.pl/demo/");
+        driver.manage().window().maximize();
 
+        // set checkin
+        driver.findElement(By.name("checkin")).sendKeys("14/09/2025");
+//        driver.findElement(By.xpath("//*[@id=\"dpd1\"]/div/input")).sendKeys("13/09/2024");
+        // set checkout
+        driver.findElement(By.name("checkout")).sendKeys("22/09/2025");
+//        driver.findElement(By.name("checkout")).click();
+//        driver.findElements(By.xpath("//td[@class='day ' and text()='30']"))
+//                .stream()
+//                .filter(WebElement::isDisplayed)       !!!!!! wybieranie z kalendarza ale tylko dzien   !!!!!
+//                .findFirst()
+//                .ifPresent(WebElement::click);
 
+        driver.findElement(By.id("travellersInput")).click();
+        // add adult
+        driver.findElement(By.id("adultPlusBtn")).click();
+        // add child
+        driver.findElement(By.id("childPlusBtn")).click();
+        // click search
+        driver.findElement(By.xpath("//button[text()=' Search']")).click();
 
+        WebElement noResult = driver.findElement(By.xpath("//*[@id=\"body-section\"]/div[5]/div[1]/div[3]/div/div/h2"));
+        System.out.println(noResult.getText());
 
+        Assert.assertTrue(noResult.isDisplayed());
+        Assert.assertEquals(noResult.getText(),"No Results Found");
 
+        driver.close();
 
     }
 }
--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -46,4 +46,89 @@ public class SignUpTest {
         driver.close();
 
     }
+
+
+    @Test
+    public void signUpWithEmptyForm() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(2L, TimeUnit.SECONDS);
+        driver.get("http://kurs-selenium.pl/demo/");
+        driver.manage().window().maximize();
+
+        driver.findElements(By.xpath("//li[@id='li_myaccount']")).stream()
+                .filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+        // click Sign UP
+        driver.findElements(By.xpath("//a[text()='  Sign Up']")).stream()    //.get(1).click();
+                .filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+
+
+        driver.findElement(By.xpath("//button[text()=' Sign Up']")).click();
+
+        WebElement emailFail = driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[2]/div/p[1]"));
+        System.out.println(emailFail.getText());
+        Assert.assertTrue(emailFail.isDisplayed());
+        Assert.assertEquals(emailFail.getText(), "The Email field is required.");
+
+        WebElement passwordFail = driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[2]/div/p[2]"));
+        System.out.println(passwordFail.getText());
+        Assert.assertTrue(passwordFail.isDisplayed());
+        Assert.assertEquals(passwordFail.getText(), "The Password field is required.");
+
+        WebElement password2Fail = driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[2]/div/p[3]"));
+        System.out.println(password2Fail.getText());
+        Assert.assertTrue(password2Fail.isDisplayed());
+        Assert.assertEquals(password2Fail.getText(), "The Password field is required.");
+
+        WebElement firstNameFail = driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[2]/div/p[4]"));
+        System.out.println(firstNameFail.getText());
+        Assert.assertTrue(firstNameFail.isDisplayed());
+        Assert.assertEquals(firstNameFail.getText(), "The First name field is required.");
+
+        WebElement lastNameFail = driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[2]/div/p[5]"));
+        System.out.println(lastNameFail.getText());
+        Assert.assertTrue(lastNameFail.isDisplayed());
+        Assert.assertEquals(lastNameFail.getText(), "The Last Name field is required.");
+
+
+
+        driver.close();
+
+    }
+
+
+    @Test
+    public void signUpWithBadEmail() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(2L, TimeUnit.SECONDS);
+        driver.get("http://kurs-selenium.pl/demo/");
+        driver.manage().window().maximize();
+
+
+        // click MY ACCOUNT
+        driver.findElements(By.xpath("//li[@id='li_myaccount']")).stream()
+                .filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+        // click Sign UP
+        driver.findElements(By.xpath("//a[text()='  Sign Up']")).stream()    //.get(1).click();
+                .filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+        // entering informations
+        driver.findElement(By.name("firstname")).sendKeys("Andrzej");
+        driver.findElement(By.name("lastname")).sendKeys("Testowany");
+        driver.findElement(By.name("phone")).sendKeys("111222333");
+        driver.findElement(By.name("email")).sendKeys("zlyemail.pl");
+        driver.findElement(By.name("password")).sendKeys("haslotestowe123");
+        driver.findElement(By.name("confirmpassword")).sendKeys("haslotestowe123");
+//        driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[9]/button")).click();
+        driver.findElement(By.xpath("//button[text()=' Sign Up']")).click();
+
+        WebElement invalidEmailInfo = driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[2]/div/p"));
+        System.out.println(invalidEmailInfo.getText());
+        Assert.assertTrue(invalidEmailInfo.isDisplayed());
+        Assert.assertEquals(invalidEmailInfo.getText(), "The Email field must contain a valid email address.");
+
+
+        driver.close();
+
+    }
 }
